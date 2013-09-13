86248978fae82aa607af14f00aa3e1b7c58fc22c
Andreas Fidjeland <andreas.fidjeland@gmail.com>
2013-09-13 15:43:31 +0100

    Torch export deals with arbitrary byte order

--- init.lua
+++ init.lua
@@ -1,5 +1,12 @@
 local pkg = require 'liboocairo'
 
+local channel = {} -- r,g,b,a
+
+for i = 1,#pkg.BYTE_ORDER do
+    channel[string.char(string.byte(pkg.BYTE_ORDER, i))] = i
+end
+
+
 
 function pkg.rgb2tensor(surface, alpha)
 
@@ -9,13 +16,18 @@ function pkg.rgb2tensor(surface, alpha)
     local w = surface:get_width()
     local img_b = torch.ByteTensor(h, w, 4)
     img_b:storage():string(data)
+
     local nchannels = 3
     if alpha == nil or alpha == true then
         nchannels = 4
     end
     local output = torch.ByteTensor(nchannels, h, w)
-    for i = 1,nchannels do
-        output[i] = img_b:select(3, i)
+
+    output[1]:copy(img_b:select(3, channel.r))
+    output[2]:copy(img_b:select(3, channel.g))
+    output[3]:copy(img_b:select(3, channel.b))
+    if nchannels == 4 then
+        output[4]:copy(img_b:select(3, channel.a))
     end
 
     return output
