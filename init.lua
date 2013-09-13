cfff98cc692a9fb76fa5765930dbd2b06e330661
Andreas Fidjeland <andreas.fidjeland@gmail.com>
2013-09-13 13:26:05 +0100

    Added rgb2tensor export function

--- init.lua
+++ init.lua
@@ -1,1 +1,13 @@
-return require 'liboocairo'
+local pkg = require 'liboocairo'
+
+function pkg.rgb2tensor(out, surface)
+    surface:flush()
+    local data = surface:get_data()
+    local size = surface:get_width()
+    assert(size == surface:get_height())
+    local img_b = torch.ByteTensor(size, size, 4)
+    img_b:storage():string(data)
+    return out:copy(img_b:select(3,2))
+end
+
+return pkg;
